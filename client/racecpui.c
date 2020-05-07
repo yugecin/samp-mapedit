b63fb12d1c0b49ddf906fd95723cd8cca769d23c
yugecin <yugecin@users.noreply.github.com>
2020-05-07 23:12:43 +0200

    Allow manipulation of checkpoint coordinates

--- client/racecpui.c
+++ client/racecpui.c
@@ -3,19 +3,59 @@
 #include "common.h"
 #include "game.h"
 #include "ui.h"
+#include "objects.h"
 #include "msgbox.h"
 #include "racecp.h"
 #include "racecpui.h"
+#include "sockets.h"
+#include "../shared/serverlink.h"
 #include <stdio.h>
+#include <math.h>
 
-struct UI_WINDOW *window_cpsettings;
+enum manipulateobject_mode {
+	MODE_NONE,
+	MODE_ORIGIN,
+	MODE_RADIUS,
+	MODE_DIRECTION,
+};
+
+enum manipulateobject_mode movemode;
+
+static struct UI_WINDOW *window_cpsettings;
 
 static struct UI_BUTTON *btn_mainmenu_cplist;
 static struct UI_BUTTON *btn_contextmenu_mkracecp;
 static struct RADIOBUTTONGROUP *rdbgroup_cptype;
 static struct UI_COLORPICKER *cp_colpick;
+static struct UI_INPUT *in_coord_x;
+static struct UI_INPUT *in_coord_y;
+static struct UI_INPUT *in_coord_z;
+static struct UI_INPUT *in_radius;
+static ui_method *proc_cpsettings_draw;
 
 static int editingCheckpoint;
+
+static
+void update_inputs_origin()
+{
+	char buf[20];
+
+	sprintf(buf, "%.4f", racecheckpoints[editingCheckpoint].pos.x);
+	ui_in_set_text(in_coord_x, buf);
+	sprintf(buf, "%.4f", racecheckpoints[editingCheckpoint].pos.y);
+	ui_in_set_text(in_coord_y, buf);
+	sprintf(buf, "%.4f", racecheckpoints[editingCheckpoint].pos.z);
+	ui_in_set_text(in_coord_z, buf);
+}
+
+static
+void update_inputs_radius()
+{
+	char buf[20];
+
+	sprintf(buf, "%.4f", racecheckpoints[editingCheckpoint].fRadius);
+	ui_in_set_text(in_radius, buf);
+}
 
 static
 void cb_btn_mkracecp(struct UI_BUTTON *btn)
@@ -39,18 +79,87 @@ void cb_btn_mkracecp(struct UI_BUTTON *btn)
 		game_ScreenToWorld(&posToCreate, x, y, 40.0f);
 	}
 
+	movemode = MODE_NONE;
 	editingCheckpoint = numcheckpoints;
 	racecheckpoints[editingCheckpoint].used = 1;
 	racecheckpoints[editingCheckpoint].free = 0;
 	racecheckpoints[editingCheckpoint].pos = posToCreate;
+	racecheckpoints[editingCheckpoint].fRadius = 2.0f;
 	racecheckpoints[editingCheckpoint].arrowDirection.x = 10.0f;
 	racecheckpoints[editingCheckpoint].arrowDirection.y = 0.0f;
 	racecheckpoints[editingCheckpoint].arrowDirection.z = 0.0f;
 	racecheckpoints[editingCheckpoint].colABGR = cp_colpick->last_selected_colorABGR;
 	ui_rdb_click_match_userdata(rdbgroup_cptype, (void*) 0);
 	numcheckpoints++;
+	update_inputs_origin();
+	update_inputs_radius();
 
 	ui_show_window(window_cpsettings);
+}
+
+static
+void cb_btn_move(struct UI_BUTTON *btn)
+{
+	struct MSG_NC nc;
+
+	movemode = MODE_ORIGIN;
+	game_ObjectSetPos(manipulateEntity, &racecheckpoints[editingCheckpoint].pos);
+	game_ObjectSetHeadingRad(manipulateEntity, 0.0f);
+	nc._parent.id = MAPEDIT_MSG_NATIVECALL;
+	nc._parent.data = 0;
+	nc.nc = NC_SetObjectRot;
+	nc.params.asint[1] = manipulateObject.samp_objectid;
+	nc.params.asflt[2] = 0.0f;
+	nc.params.asflt[3] = 0.0f;
+	nc.params.asflt[4] = 0.0f;
+	sockets_send(&nc, sizeof(nc));
+	nc._parent.id = MAPEDIT_MSG_NATIVECALL;
+	nc._parent.data = 0;
+	nc.nc = NC_EditObject;
+	nc.params.asint[1] = 0;
+	nc.params.asint[2] = manipulateObject.samp_objectid;
+	sockets_send(&nc, sizeof(nc));
+}
+
+static
+void cb_btn_move_radius(struct UI_BUTTON *btn)
+{
+	struct MSG_NC nc;
+	struct RwV3D pos;
+
+	movemode = MODE_RADIUS;
+	pos = racecheckpoints[editingCheckpoint].pos;
+	pos.x += racecheckpoints[editingCheckpoint].fRadius;
+	game_ObjectSetPos(manipulateEntity, &pos);
+	game_ObjectSetHeadingRad(manipulateEntity, 0.0f);
+	nc._parent.id = MAPEDIT_MSG_NATIVECALL;
+	nc._parent.data = 0;
+	nc.nc = NC_SetObjectRot;
+	nc.params.asint[1] = manipulateObject.samp_objectid;
+	nc.params.asflt[2] = 0.0f;
+	nc.params.asflt[3] = 0.0f;
+	nc.params.asflt[4] = 0.0f;
+	sockets_send(&nc, sizeof(nc));
+	nc._parent.id = MAPEDIT_MSG_NATIVECALL;
+	nc._parent.data = 0;
+	nc.nc = NC_EditObject;
+	nc.params.asint[1] = 0;
+	nc.params.asint[2] = manipulateObject.samp_objectid;
+	sockets_send(&nc, sizeof(nc));
+}
+
+static
+void cb_in_origin(struct UI_INPUT *in)
+{
+	*(&racecheckpoints[editingCheckpoint].pos.x + (int) in->_parent.userdata) = (float) atof(in->value);
+}
+
+static
+void cb_in_radius(struct UI_INPUT *in)
+{
+	racecheckpoints[editingCheckpoint].fRadius = (float) atof(in->value);
+	racecheckpoints[editingCheckpoint].used = 0;
+	racecheckpoints[editingCheckpoint].free = 1;
 }
 
 static
@@ -91,6 +200,42 @@ void cb_btn_cpreset(struct UI_BUTTON *btn)
 	ui_rdb_click_match_userdata(rdbgroup_cptype, (void*) 0);
 }
 
+static
+int draw_window_cpsettings(struct UI_ELEMENT *wnd)
+{
+	struct RwV3D pos;
+	float dx, dy, oldrad;
+
+	if (movemode == MODE_ORIGIN) {
+		if (ui_active_element == in_coord_x ||
+			ui_active_element == in_coord_y ||
+			ui_active_element == in_coord_z)
+		{
+			movemode = MODE_NONE;
+			goto skip;
+		}
+		game_ObjectGetPos(manipulateEntity, &racecheckpoints[editingCheckpoint].pos);
+		update_inputs_origin();
+	} else if (movemode == MODE_RADIUS) {
+		if (ui_active_element == in_radius) {
+			movemode = MODE_NONE;
+			goto skip;
+		}
+		game_ObjectGetPos(manipulateEntity, &pos);
+		dx = pos.x - racecheckpoints[editingCheckpoint].pos.x;
+		dy = pos.y - racecheckpoints[editingCheckpoint].pos.y;
+		oldrad = racecheckpoints[editingCheckpoint].fRadius;
+		racecheckpoints[editingCheckpoint].fRadius = (float) sqrt(dx * dx + dy * dy);
+		if (fabs(oldrad - racecheckpoints[editingCheckpoint].fRadius) > 0.01f) {
+			racecheckpoints[editingCheckpoint].used = 0;
+			racecheckpoints[editingCheckpoint].free = 1;
+		}
+		update_inputs_radius();
+	}
+skip:
+	return proc_cpsettings_draw(wnd);
+}
+
 void racecpui_init()
 {
 	struct UI_BUTTON *btn;
@@ -116,8 +261,36 @@ void racecpui_init()
 	/*checkpoint settings window*/
 	window_cpsettings = ui_wnd_make(9000.0f, 500.0f, "Checkpoint");
 	window_cpsettings->columns = 4;
-	lbl = ui_lbl_make("Type:");
-	ui_wnd_add_child(window_cpsettings, lbl);
+	proc_cpsettings_draw = window_cpsettings->_parent._parent.proc_draw;
+	window_cpsettings->_parent._parent.proc_draw = draw_window_cpsettings;
+	ui_wnd_add_child(window_cpsettings, ui_lbl_make("Origin:"));
+	in_coord_x = ui_in_make(cb_in_origin);
+	in_coord_x->_parent.span = 3;
+	in_coord_x->_parent.userdata = (void*) 0;
+	ui_wnd_add_child(window_cpsettings, in_coord_x);
+	ui_wnd_add_child(window_cpsettings, NULL);
+	in_coord_y = ui_in_make(cb_in_origin);
+	in_coord_y->_parent.span = 3;
+	in_coord_y->_parent.userdata = (void*) 1;
+	ui_wnd_add_child(window_cpsettings, in_coord_y);
+	ui_wnd_add_child(window_cpsettings, NULL);
+	in_coord_z = ui_in_make(cb_in_origin);
+	in_coord_z->_parent.span = 3;
+	in_coord_z->_parent.userdata = (void*) 2;
+	ui_wnd_add_child(window_cpsettings, in_coord_z);
+	ui_wnd_add_child(window_cpsettings, NULL);
+	btn = ui_btn_make("Move", cb_btn_move);
+	btn->_parent.span = 3;
+	ui_wnd_add_child(window_cpsettings, btn);
+	ui_wnd_add_child(window_cpsettings, ui_lbl_make("Radius:"));
+	in_radius = ui_in_make(cb_in_radius);
+	in_radius->_parent.span = 3;
+	ui_wnd_add_child(window_cpsettings, in_radius);
+	ui_wnd_add_child(window_cpsettings, NULL);
+	btn = ui_btn_make("Move", cb_btn_move_radius);
+	btn->_parent.span = 3;
+	ui_wnd_add_child(window_cpsettings, btn);
+	ui_wnd_add_child(window_cpsettings, ui_lbl_make("Type:"));
 	rdbgroup_cptype = ui_rdbgroup_make(cb_rdbgroup_cptype);
 	rdb = ui_rdb_make("0._Arrow", rdbgroup_cptype, 1);
 	rdb->_parent._parent.span = 3;
